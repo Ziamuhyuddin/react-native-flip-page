81ab670291d3fd45e7a9e6980a78026d5a18d2bc
zia mohe uddin <58113249+Ziamuhyuddin@users.noreply.github.com>
2024-07-18 12:37:02 +0500

    REMOVED: unNecessary console & code.

--- src/index.js
+++ src/index.js
@@ -81,11 +81,9 @@ class FlipPage extends React.Component {
 
       //if there is a double tap within delay time and there is no dragging with finger 
       //then this handles zoom in or zoom out for double tap
-    console.log("dx: ", Math.abs(Math.abs(touches?.[0]?.pageX)), this.lastTapX, xDiff);
 
       if (this.lastTapRef && ((now - this.lastTapRef) < DOUBLE_TAP_DELAY) && !this.hasMoved && xDiff < 20 && yDiff < 20) {
         // Double tap detected
-        console.log("inside");
         this.lastTapRef = null;
         if (this.scaleRef > 1) {
           this.isDoubleTappedZoomOut = true;
@@ -146,11 +144,9 @@ class FlipPage extends React.Component {
             prevDistance: distance,
           });
         }
-        console.log("here");
         // this.handleDoubleTap(evt, gestureState);
         // this.handleSingleTap(evt, gestureState);
         if ((Math.abs(this.state.angle) >= 5 && (this.state.animating || this.state.direction)) || this.hasEnd){
-          console.log("FALSE");
           return false;
         } else {
         return true;
@@ -240,9 +236,7 @@ class FlipPage extends React.Component {
     const { orientation, loopForever, reverse } = this.props;
     const dn = orientation === "vertical" ? dy : dx;
     const touches = e.nativeEvent.touches;
-
-    console.log("moving");
-
+    
     //this store the movement 
     if (touches?.length == 1) {
       this.oneFingerRemoved = true;
@@ -429,7 +423,6 @@ class FlipPage extends React.Component {
         }, 300);
     } else this.allowPinch = true;
 
-    // console.log("lastDx dx: ", this.lastDx, Math.abs(dx), Math.abs(this.lastDx - Math.abs(dx)));
 
     //if there are two fingers then this zoom in zoom out or pinch in or pinch out
    // if (touches?.length >= 2 && !this.state.direction && !this.isDoubleTapped && (this.oneFingerRemoved ? Math.abs(this.lastDx - Math.abs(dx)) > 15 : this.allowPinch)) 
@@ -738,122 +731,6 @@ class FlipPage extends React.Component {
     }, 10);
   }
   
-  
-//   resetHalves() {
-//     const { loopForever, children } = this.props;
-//     const pages = children.length;
-//     const { angle, direction, shouldGoNext, shouldGoPrevious, page } =
-//       this.state;
-
-//     const firstHalf = this.firstHalves[page];
-//     const secondHalf = this.secondHalves[page];
-
-//     const finish = () => {
-//       const { onFinish } = this.props;
-//       const { direction } = this.state;
-//       this.setState({ direction: "" });
-
-//       this.state.animating = true;
-//       console.log("BEFORE");
-// //Timeout to prevent the page from flipping back immediately
-//       setTimeout(() => {
-//         console.log("AFTER");
-     
-//         this.state.animating = false;
-//       }, 500);
-
-//       if (shouldGoNext) {
-//         this.setState(
-//           {
-//             angle: 0,
-//             page: loopForever && this.isOnLastPage() ? 0 : page + 1,
-//           },
-//           () => {
-//             this.allowZoom && this.props.onPageChange(this.state.page, "next");
-//             this.allowZoom && firstHalf.setNativeProps({ transform: [] });
-//             this.allowZoom && secondHalf.setNativeProps({ transform: [] });
-//           }
-//         );
-//       } else if (shouldGoPrevious) {
-//         this.setState(
-//           {
-//             angle: 0,
-//             page: loopForever && this.isOnFirstPage() ? pages - 1 : page - 1,
-//           },
-//           () => {
-//             this.allowZoom && this.props.onPageChange(this.state.page, "prev");
-//             this.allowZoom && firstHalf.setNativeProps({ transform: [] });
-//             this.allowZoom && secondHalf.setNativeProps({ transform: [] });
-//           }
-//         );
-//       } else {
-//         //this call onFinish function only
-//         //where scale ref is 1
-//         //and there is swipe towards next page
-//         if (typeof onFinish === "function" && this.allowZoom && Math.abs(angle) > 15) {
-//           onFinish(direction);
-//         }
-//       }
-//     };
-
-//     // Already swiped all the way
-//     if (Math.abs(angle) === 180) {
-//       finish();
-//       return;
-//     }
-
-//     let targetAngle;
-//     if (angle < -90) {
-//       targetAngle = -180;
-//     } else if (angle > 90) {
-//       targetAngle = 180;
-//     } else {
-//       targetAngle = 0;
-//     }
-
-//     this.resetTimer = setInterval(() => {
-//       let { angle } = this.state;
-
-//       angle += angle < targetAngle ? 5 : -5;
-
-//       if (angle < 0) {
-//         angle = Math.max(angle, -180);
-//       } else {
-//         angle = Math.min(angle, 180);
-//       }
-
-//       let matrix = rotateX(angle);
-
-//       if (angle < 0) {
-//         this.rotateSecondHalf(angle);
-//       } else {
-//         this.rotateFirstHalf(angle);
-//       }
-
-//       this.setState({ angle });
-
-//       if (
-//         (targetAngle < 0 && angle <= targetAngle) || // Flip second half to top
-//         (targetAngle === 0 && Math.abs(angle) <= 5) ||
-//         (targetAngle > 0 && angle >= targetAngle) // Flip first half to bottom
-//       ) {
-//         clearInterval(this.resetTimer);
-
-//         if (direction === "top" || direction === "left" || direction === "") {
-//           this.rotateSecondHalf(targetAngle);
-//         } else if (
-//           direction === "bottom" ||
-//           direction === "right" ||
-//           direction === ""
-//         ) {
-//           this.rotateFirstHalf(targetAngle);
-//         }
-
-//         finish();
-//       }
-//     }, 10);
-//   }
-
   handlePanResponderStop(e, gestureState) {
     const { dx, dy, numberActiveTouches } = gestureState;
     const { angle, page, direction } = this.state;
